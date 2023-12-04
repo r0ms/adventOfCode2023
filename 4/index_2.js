15080273eb6a607192280f6fe66c763485fdaea7
Romain Bontemps <romain.bontemps@payfit.com>
2023-12-04 12:58:18 +0100

    aoc(day4): added part 2

--- 4/index_2.js
+++ 4/index_2.js
@@ -1,11 +1,17 @@
 const data =  require('./data.json')
 const CARD_REMOVAL_REGEX = /^Card\W+\d+:/g
 const i = (a, b) => (a.filter(Set.prototype.has, new Set(b)))
-const r = data.map((l) => {
+const wins = new Array(data.length).fill(1)
+data.map((l) => {
   let arrs = l.replace(CARD_REMOVAL_REGEX, '').trimStart().trimEnd().split(' | ')
   .map((e) => {
     return e.split(/\W+/)
   })
   return i(arrs[1], arrs[0]).length
-}).map((win) => (win?(1<<win-1):0)).reduce((a, b)=>a+b, 0)
+})
+.forEach((n, index, arr) => {
+  const removed = wins.splice(index+1, n)
+  wins.splice(index+1, 0, ...removed.map((value) => value+wins[index]))
+})
+const r = wins.reduce((a,b) => a+b, 0)
 console.log('result', r)
